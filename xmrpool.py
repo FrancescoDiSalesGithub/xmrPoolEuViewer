548e83d8e2d126476bb06ac80c82be4929cfdde1
Francesco Di Sales <f.disales@hotmail.com>
2022-08-17 08:06:13 -0400

    Create xmrpool.py

---
+++ xmrpool.py
@@ -0,0 +1,26 @@
+class xmrpool:
+
+    def __init__(self,stats,worker,payments):
+        self.stats = stats
+        self.worker = worker
+        self.payments = payments
+
+    def get_balance(self):
+        return self.stats['balance']
+
+    def get_payments(self):
+        return self.payments
+
+    def get_workers(self):
+        return self.worker
+
+    def get_list_workers(self):
+        print("workers: ")
+        for value in self.worker:
+            print("worker id: " + value['workerId']+ " hashes: " + value['hashes'] + " last share: " + value['lastShare'] + " expired: "+value['expired'])
+
+    def get_list_payments(self):
+        print("payments: ")
+        for pays in self.payments:
+            print(pays)
+
